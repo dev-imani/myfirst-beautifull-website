dcc19339cf8f98c03043d8cb2eec293dbd554a0d
mamesareagan <mamesareagan0@gmail.com>
2025-02-05 15:29:40 -0500

    routes for the brand endpoints

--- Shoe-site-Backend/shoeshop/products/urls.py
+++ Shoe-site-Backend/shoeshop/products/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from products.views import CategoryViewSet
+from products.views import CategoryViewSet, BrandViewSet
 
 # Set the app name for namespacing
 app_name = "products"
 
 router = routers.DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"brands", BrandViewSet, basename="brands" )
 # Define URL patterns for the 'users' app
 urlpatterns = [
     path("", include(router.urls)),
