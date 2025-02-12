7dd324d9d2f435c5b425b328bcb9cf26c9c02239
mamesareagan <mamesareagan0@gmail.com>
2025-02-12 01:59:44 -0500

    products path

--- Shoe-site-Backend/shoeshop/products/urls.py
+++ Shoe-site-Backend/shoeshop/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from products.views import CategoryViewSet, BrandViewSet
+from products.views import CategoryViewSet, BrandViewSet, ProductViewSet
 
 # Set the app name for namespacing
 app_name = "products"
@@ -9,6 +9,7 @@ app_name = "products"
 router = routers.DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="categories")
 router.register(r"brands", BrandViewSet, basename="brands" )
+router.register(r'products', ProductViewSet, basename='product')
 # Define URL patterns for the 'users' app
 urlpatterns = [
     path("", include(router.urls)),
